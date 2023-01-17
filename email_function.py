8520a8324db57f787ce9c9af4e79482255b481ad
Genone22 <100020872+Genone22@users.noreply.github.com>
2023-01-17 10:13:55 +0300

    Update email_function.py

--- email_function.py
+++ email_function.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
 import smtplib
-# from email.mime.text import MIMEText
 
 
 def emails_send_funct(to_, subj_, msg_, from_, pass_):
-    # print(to_, subj_, msg_, from_, pass_)
-
     # Create session for mail.ru
-    s = smtplib.SMTP('smtp.yandex.ru', 587)  # 587
+    s = smtplib.SMTP('smtp.yandex.ru', 587)
     # Start TLS for security
     s.starttls()
     # Authentication
     s.login(from_, pass_)
     # Message to be sent
-    msg = MIMEText(msg_)
-    msg = 'Subject: {}\n\n{}'.format(subj_, msg_)  # , encoding='utf-8'
+    msg = 'Subject: {}\n\n{}'.format(subj_, msg_)
     # Sending the mail
     s.sendmail(from_, to_, msg_)  # , msg_.as_string()
     x = s.ehlo()
