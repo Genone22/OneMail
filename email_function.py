5136c7286e70d47ca0c009847e1282cbf9e1071d
Genone22 <100020872+Genone22@users.noreply.github.com>
2023-01-18 10:04:02 +0300

    Update email_function.py

--- email_function.py
+++ email_function.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import smtplib
+from email.mime.text import MIMEText
 
 
 def emails_send_funct(to_, subj_, msg_, from_, pass_):
@@ -10,9 +11,12 @@ def emails_send_funct(to_, subj_, msg_, from_, pass_):
     # Authentication
     s.login(from_, pass_)
     # Message to be sent
-    msg = 'Subject: {}\n\n{}'.format(subj_, msg_)
+    msg = MIMEText(msg_, "plain", "utf-8")
+    msg['Subject'] = subj_
+    msg['From'] = from_
+    msg['To'] = to_
     # Sending the mail
-    s.sendmail(from_, to_, msg_)  # , msg_.as_string()
+    s.sendmail(from_, to_, msg.as_string())
     x = s.ehlo()
     if x[0] == 250:
         return 's'
